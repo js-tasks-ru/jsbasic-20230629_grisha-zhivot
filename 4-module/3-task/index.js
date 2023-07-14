4befe51940494e4bd262528cda54208695207195
grisha <grisha.zhivot@bk.ru>
2023-07-14 16:43:27 +0300

    4-module-1-task 4-module-2-task 4-module-3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.rows;
+
+  for(let i = 0; i < rows.length; i += 1) {
+    if(rows[i].cells[3].dataset.available == 'true') {
+      rows[i].classList.add('available');
+    } 
+    else if(rows[i].cells[3].dataset.available == 'false') {
+      rows[i].classList.add('unavailable');
+    } 
+    else {
+      rows[i].hidden = true
+    }
+  }
+  for(let i = 0; i < rows.length; i += 1) {
+    if(rows[i].cells[2].textContent == 'm') {
+      rows[i].classList.add('male');
+    }
+    else {
+      rows[i].classList.add('female');
+    }
+  }
+  for(let i = 0; i < rows.length; i += 1) {
+    if(rows[i].cells[1].textContent < 18) {
+      rows[i].style.textDecoration = 'line-through'
+    }
+  }
+  return table;
 }
